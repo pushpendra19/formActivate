eda4c167521c4538031604a5a97b830dc464b5d8
Pushpendra Awasthi <pushpendra19@gmail.com>
2012-05-02 00:28:36 +0530

    Comment:test

--- validate_phone_number.php
+++ validate_phone_number.php
@@ -4,5 +4,5 @@
 ?>
 <Response>
     <Pause length="1"/><Say voice="woman">A customer at the number </Say><Pause length="1"/><Say voice="woman"><?php echo @$_REQUEST['number']?> has submitted an inquiry.</Say>
-    <Dial><?php echo $_REQUEST['number']?></Dial>
+    <Dial><?php echo $_REQUEST['number']?></Dial>   
 </Response>
